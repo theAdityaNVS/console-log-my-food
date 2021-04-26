50944d5fbd4606c1d5977a86edf7ceb16773bf50
N V S Aditya <aditya.nadamuni@gmail.com>
2021-04-26 14:19:16 +0530

    second commit

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 #! /usr/bin/env node
+const readline = require(readline).createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
+readline.question(`What would you like to log today? `, (item) => {
+    console.log(item),
+    readline.close()
+});
